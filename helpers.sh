bf0f7f68164fab3c9eeb6dfc48c2efd94aa58494
Andrew Albers <asalbers@gmail.com>
2021-07-22 16:30:23 -0500

    added random method

--- helpers.sh
+++ helpers.sh
@@ -1,6 +1,11 @@
 #!/usr/bin/env bash
 set -euo pipefail
 
+random-string() {
+  # remove the damned space
+        cat /dev/urandom | tr -dc 'a-zA-Z0-9' | fold -w ${1:-8} | head -n 1 | tr '[:upper:]' '[:lower:]'
+}
+
 install() {
   echo Hello World
 }
