8814bec1d3864c40a8f4632e7557cf76b130326d
Andrew Albers <asalbers@gmail.com>
2021-07-22 16:30:23 -0500

    added random method

--- helpers.sh
+++ helpers.sh
@@ -6,6 +6,11 @@ random_string() {
         cat /dev/urandom | tr -dc 'a-zA-Z0-9' | fold -w ${1:-8} | head -n 1 | tr '[:upper:]' '[:lower:]'
 }
 
+random-string() {
+  # remove the damned space
+        cat /dev/urandom | tr -dc 'a-zA-Z0-9' | fold -w ${1:-8} | head -n 1 | tr '[:upper:]' '[:lower:]'
+}
+
 install() {
   echo Hello World
 }
